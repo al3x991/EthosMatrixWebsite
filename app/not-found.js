7623596a12af435ab4045f4fca55f2f988ed3118
al3x991 <ostrategic@gmail.com>
2023-08-25 12:18:18 +0100

    foundation 95%

--- app/not-found.js
+++ app/not-found.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function NotFound() {
   return <div className='container mx-auto'>
     <div className=" flex flex-col items-center justify-center pt-20">
-    <Image 
+    <img 
 src="https://i.giphy.com/media/PvcOp93XdMoQ6S6IiV/giphy.webp"
 blurDataURL="data:..."
 placeholder="blur"
