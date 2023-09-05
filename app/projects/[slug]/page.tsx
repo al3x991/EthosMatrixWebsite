5d97a262fc8a9629c76345ced093daff36809799
Omoruyi Osaretin A <42362272+al3x991@users.noreply.github.com>
2023-09-05 15:39:23 +0000

    Update page.tsx

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,6 +1,10 @@
-import { FC } from 'react'
-import { findProjectBySlug } from '../../utils/projectData'
+import { FC, React } from 'react';
+import { findProjectBySlug } from '../../utils/projectData';
 import projectsData from '../../data/projects.json'; // Adjust the import path to match your project structure
+import PageHeader from '../../components/PageHeader';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer'
+
 
 interface PageProps {
     params: {
@@ -18,7 +22,6 @@ const ProjectPage: FC<pageProps> =  ({params}) => {
     const project = findProjectBySlug(params.slug);
    
     if (!project) {
-        console.log(project);
       return <div>Project not found</div>;
     }
   
@@ -38,11 +41,55 @@ const ProjectPage: FC<pageProps> =  ({params}) => {
     } = project;
   
     return (
-      <div>
-        <h1>{project.title}</h1>
-        <p>{project.description}</p>
-       
+        <>
+<Navbar/>
+<PageHeader title='Project Details' imgSource={"/assets/images/gallery-hero.webp"} curL='Gallery'/>
+    
+<div className="bg-gray-100 min-h-screen p-4">
+      <div className="max-w-screen-lg mx-auto bg-white rounded-lg shadow-lg p-6">
+        <header className="text-center bg-blue-500 text-white py-4 px-6 rounded-t-lg">
+          <h1 className="text-4xl font-bold">{project.title}</h1>
+          <p className="text-lg">{project.category}</p>
+          <p className="text-base">{project.description}</p>
+        </header>
+        <section className="p-6">
+          <div className="flex justify-between mb-6">
+            <div>
+              <p className="font-semibold">Scopes:</p>
+              <ul className="list-disc list-inside">
+                {project.scopes.map((scope, index) => (
+                  <li key={index}>{scope}</li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <p className="font-semibold">Date:</p>
+              <p>{project.date}</p>
+            </div>
+            <div>
+              <p className="font-semibold">Client:</p>
+              <p>{project.client}</p>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            {project.images.map((image, index) => (
+              <div
+                key={index}
+                className="relative group overflow-hidden transition-transform transform hover:scale-105"
+              >
+                <img
+                  src={images}
+                  alt={`Image ${index + 1}`}
+                  className="w-full h-auto"
+                />
+              </div>
+            ))}
+          </div>
+        </section>
       </div>
+    </div>
+      <Footer/>
+      </>
     );
   };
   
