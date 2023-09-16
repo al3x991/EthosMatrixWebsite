505c4a8127838845952ed9129b1aa01dac1b61b4
al3x991 <ostrategic@gmail.com>
2023-09-16 23:26:34 +0100

    Fixx

--- app/utils/projectData.ts
+++
@@ -1,7 +0,0 @@
-// /app/utils/projectData.ts
-import projectsData from '../data/projects.json'
-
-
-export function findProjectBySlug(slug) {
-    return projectsData.projects.find((project) => project.slug === slug) || null;
-  }
